bdc81cdf896475b1ec3627a914a1182a0fa6c013
Ta_Yu <dylan.chen600@gmail.com>
2025-05-15 16:54:40 +0800

    code架構，大部分為空的

---
+++ assets/scripts/UI/StartUI.ts
@@ -0,0 +1,51 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class StartUI extends cc.Component {
+    @property(cc.Button)
+    startButton: cc.Button = null;
+
+    @property(cc.Button)
+    multiplayerButton: cc.Button = null;
+
+    @property(cc.Button)
+    settingButton: cc.Button = null;
+
+    @property(cc.Button)
+    quitButton: cc.Button = null;
+
+    onLoad() {
+        if (this.startButton) {
+            this.startButton.node.on('click', this.onClickStart, this);
+        }
+        if (this.multiplayerButton) {
+            this.multiplayerButton.node.on('click', this.onClickMultiplayer, this);
+        }
+        if (this.settingButton) {
+            this.settingButton.node.on('click', this.onClickSetting, this);
+        }
+        if (this.quitButton) {
+            this.quitButton.node.on('click', this.onClickQuit, this);
+        }
+    }
+
+    onClickStart() {
+        console.log("▶️ 單人模式啟動！");
+        cc.director.loadScene("GameBuildScene");
+    }
+
+    onClickMultiplayer() {
+        console.log("🧑‍🤝‍🧑 多人模式啟動！");
+        // cc.director.loadScene("RoomScene"); // 實作後可導向房間
+    }
+
+    onClickSetting() {
+        console.log("⚙️ 設定畫面打開！");
+        // 可打開設定面板
+    }
+
+    onClickQuit() {
+        console.log("🛑 離開遊戲！");
+        // window.close(); // 僅原生 App 有效
+    }
+}
